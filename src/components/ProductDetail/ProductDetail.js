408e69f2cdf0e0eefda71afe27582863d882d142
Md-Sujon <mdsujon829562@gmail.com>
2021-03-29 02:12:26 +0600

    ema jhon client site

---
+++ src/components/ProductDetail/ProductDetail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Product from '../Product/Product';
+
+const ProductDetail = () => {
+    const {productKey} = useParams();
+    const [product,setProduct]=useState({});
+
+    useEffect(() =>{
+        fetch('https://desolate-cliffs-97513.herokuapp.com/product/'+ productKey)
+        .then(res=>res.json())
+        .then(data=>setProduct(data));
+    },[productKey])
+ return (
+        <div>
+            <h1>Your Product Details.</h1>
+            <Product showAddToCart={false} product={product}></Product>
+        </div>
+    );
+};
+
+export default ProductDetail;
